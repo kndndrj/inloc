f97c3531eb22ba363ff4a46f207cb217f181b03c
Andrej Kenda <andrej.baca14@gmail.com>
2024-08-27 21:17:33 +0200

    added base64 example

---
+++ base64/README.md
@@ -0,0 +1,25 @@
+# Base64 Encoder/Decoder
+
+| Language | Lines of Code |
+| :------: | :-----------: |
+|  Python  |      37       |
+
+## Running
+
+Install python and use this script the same way as the cli tool:
+
+```sh
+echo "This is a test." | python base64.py | python base64.py -d
+```
+
+The script should work on any data.
+
+## Sources
+
+Loosely based on code from [this repository](https://github.com/leon73/Python-Base64).
+
+## License
+
+The original source is licensed under `CC0 1.0 Universal`, but I think that I changed the code so
+much that it can have it's own license. Therefore, this code is licensed with the license from
+[parent repository](../LICENSE).
---
+++ base64/base64.py
@@ -0,0 +1,65 @@
+_TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+
+def encode(body: bytes) -> str:
+    # convert input to bits
+    bits = str()
+    for b in body:
+        bits += f"{b:08b}"
+
+    # calculate ammount of padding characters ("=")
+    length = len(bits)
+    while length % 6:
+        length += 8
+    padding = int((length - len(bits)) / 6)
+
+    # assemble base64 string from bits
+    encoded = str()
+    for i in range(0, len(bits), 6):
+        chunk = bits[i : i + 6]
+
+        # at the end, if the chunk is incomplete, add zeroes to it's tail
+        if len(chunk) < 6:
+            chunk += "0" * (6 - len(chunk))
+
+        # take the appropriate character from the lookup
+        encoded += _TABLE[int(chunk, 2)]
+
+    # finally, add padding characters ("=")
+    return encoded + "=" * padding
+
+
+def decode(body: str) -> bytes:
+    # assemble bits from input characters with lookup
+    # ignore padding and any possible whitespace
+    bits = str()
+    for c in body:
+        if c not in ("=", " ", "\t", "\n"):
+            bits += f"{_TABLE.index(c):06b}"
+
+    # assemble an array of bytes from produced bits
+    decoded = bytearray()
+    for i in range(0, len(bits), 8):
+        byte = bits[i : i + 8]
+
+        # at the end, if the byte is incomplete, it's leftover padding (ignore it)
+        if len(byte) < 8:
+            continue
+        decoded.append(int(byte, 2))
+
+    return bytes(decoded)
+
+
+if __name__ == "__main__":
+    import sys
+    import textwrap
+
+    if "-d" in sys.argv:
+        # strip trailing newline from input
+        dec = decode("\n".join(sys.stdin.readlines()).removesuffix("\n"))
+        # print without a newline
+        sys.stdout.buffer.write(dec)
+    else:
+        enc = encode(b"".join(sys.stdin.buffer.readlines()))
+        # print with 76 character line wrap
+        print(textwrap.fill(enc, 76))
